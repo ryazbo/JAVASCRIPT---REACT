8246ad68392dd7020a44da5522c26aaea8109645
JuanS <126506142+ryazbo@users.noreply.github.com>
2024-02-05 16:48:20 -0500

    dwq

--- Fibonacci/app.js
+++ Fibonacci/app.js
@@ -1,31 +1,25 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const n = 100;
-    const fibonacciSequence = generateFibonacciSequence(n);
-
+document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('fibonacci');
-    formulario.addEventListener('submit', function (event) {
+
+    formulario.addEventListener('submit', (event) => {
         event.preventDefault();
 
         const numUser = parseInt(document.getElementById('numUser').value);
-        const result = searchInFibonacciSequence(fibonacciSequence, numUser);
+        const n = 100;
+        const result = fibonacci(n, numUser);
 
-        if (result) {
-            console.log(result);
-            document.getElementById('outPutFibonacci').innerHTML = 'Elemento encontrado dentro de la lista';
-        } else {
-            document.getElementById('outPutFibonacci').innerHTML = 'Elemento no encontrado dentro de la lista';
-        }
+        const outputElement = document.getElementById('outputFibonacci');
+        outputElement.innerHTML = result ? 'Elemento encontrado dentro de la lista' : 'Elemento no encontrado dentro de la lista';
     });
 
-    function generateFibonacciSequence(n) {
+    function fibonacci(n, numUser) {
         let numeros = [0, 1];
         for (let i = 2; i <= n; i++) {
             numeros[i] = numeros[i - 1] + numeros[i - 2];
+            if (numeros[i] === numUser) {
+                return true;
+            }
         }
-        return numeros;
-    }
-
-    function searchInFibonacciSequence(sequence, numUser) {
-        return sequence.includes(numUser);
+        return false;
     }
 });
